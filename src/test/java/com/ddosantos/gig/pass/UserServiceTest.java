d059619748abf1765b19251f07848d58f585366e
federico <federicoberto708@gmail.com>
2025-01-21 15:11:24 +0700

    test(userService) : add unit test for login method

--- src/test/java/com/ddosantos/gig/pass/UserServiceTest.java
+++ src/test/java/com/ddosantos/gig/pass/UserServiceTest.java
@@ -1,8 +1,13 @@
 package com.ddosantos.gig.pass;
 
+import com.ddosantos.gig.pass.dto.request.UserLoginDTO;
 import com.ddosantos.gig.pass.dto.request.UserRegistrationDTO;
+import com.ddosantos.gig.pass.dto.response.UserLoginResponseDTO;
 import com.ddosantos.gig.pass.dto.response.UserRegistrationResponseDTO;
 import com.ddosantos.gig.pass.entity.User;
+import com.ddosantos.gig.pass.exception.EmailAlreadyExistException;
+import com.ddosantos.gig.pass.exception.ResourceNotFoundException;
+import com.ddosantos.gig.pass.exception.UnauthorizedException;
 import com.ddosantos.gig.pass.repository.UserRepository;
 import com.ddosantos.gig.pass.security.TokenProvider;
 import com.ddosantos.gig.pass.service.UserService;
@@ -11,15 +16,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -55,11 +59,80 @@ public class UserServiceTest {
         when(passwordEncoder.encode(dto.getPassword())).thenReturn("hashedPassword");
 
         UserRegistrationResponseDTO responseDTO = userService.register(dto);
-        verify(userRepository, Mockito.times(1)).createUser(any(User.class));
+        verify(userRepository, times(1)).createUser(any(User.class));
 
         Assertions.assertNotNull(responseDTO);
         Assertions.assertEquals("Successfully registered new user", responseDTO.getMessage());
         verify(passwordEncoder).encode(dto.getPassword());
+    }
+
+    @Test
+    void testRegister_ShouldReturnEmailAlreadyException_WhenUserEmailIsAlreadyExist() {
+        UserRegistrationDTO dto = createUserRegistrationDTO();
+
+        User existingUser = new User();
+        existingUser.setEmail(dto.getEmail());
+
+        when(userRepository.findUserByEmail(dto.getEmail())).thenReturn(existingUser);
+
+        Assertions.assertThrows(EmailAlreadyExistException.class, () -> {
+            userService.register(dto);
+        });
+
+        verify(userRepository, never()).createUser(any(User.class));
+    }
+
+    @Test
+    void testLogin_ShouldReturnLoginResponseDTO_WhenUserLoginSuccessfully() {
+        UserLoginDTO loginDTO = createUserLoginDTO();
+        User user = new User();
+        user.setEmail(loginDTO.getEmail());
+        user.setPassword(loginDTO.getPassword());
+
+        when(userRepository.findUserByEmail(loginDTO.getEmail())).thenReturn(user);
+
+        Authentication mockAuthenticate = mock(Authentication.class);
+
+        when(authenticationManager.authenticate(any(Authentication.class))).thenReturn(mockAuthenticate);
+
+        when(tokenProvider.createToken(mockAuthenticate)).thenReturn("jwt-token");
+
+        UserLoginResponseDTO responseDTO = userService.login(loginDTO);
+
+        Assertions.assertNotNull(responseDTO);
+        Assertions.assertEquals("jwt-token", responseDTO.getToken());
+    }
+
+    @Test
+    void testLogin_ShouldReturnResourceNotFoundException_WhenUserEmailNotExist() {
+        UserLoginDTO loginDTO = createUserLoginDTO();
+
+        when(userRepository.findUserByEmail(loginDTO.getEmail())).thenReturn(null);
+
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            userService.login(loginDTO);
+        });
+    }
+
+    @Test
+    void testLogin_ShouldReturnUnauthorizedException_WhenUserCredentialsIsInvalid() {
+        UserLoginDTO loginDTO = createUserLoginDTO();
+        User user = new User();
+        user.setEmail(loginDTO.getEmail());
+        user.setPassword("correct-password");
+
+        when(userRepository.findUserByEmail(loginDTO.getEmail())).thenReturn(user);
+
+        Authentication authentication = mock(Authentication.class);
+
+        when(authenticationManager.authenticate(any(Authentication.class))).
+                thenThrow(new BadCredentialsException("Invalid email or password"));
+
+        UnauthorizedException exception = Assertions.assertThrows(UnauthorizedException.class, () -> {
+            userService.login(loginDTO);
+        });
+
+        Assertions.assertEquals("Invalid email or password", exception.getMessage());
     }
 
     private UserRegistrationDTO createUserRegistrationDTO() {
@@ -69,5 +142,13 @@ public class UserServiceTest {
         userDTO.setPassword(PASSWORD);
 
         return userDTO;
+    }
+
+    private UserLoginDTO createUserLoginDTO() {
+        UserLoginDTO loginDTO = new UserLoginDTO();
+        loginDTO.setEmail(EMAIL);
+        loginDTO.setPassword(PASSWORD);
+
+        return loginDTO;
     }
 }
