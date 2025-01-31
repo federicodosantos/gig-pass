435e12fce5a44440de696df11a72f14cee3cf26e
federico <federicoberto708@gmail.com>
2025-01-31 21:13:08 +0700

    fix(test) : add test container to fix flyway connection error

--- src/test/java/com/ddosantos/gig/pass/GigPassApplicationTests.java
+++ src/test/java/com/ddosantos/gig/pass/GigPassApplicationTests.java
@@ -1,10 +1,21 @@
 package com.ddosantos.gig.pass;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Testcontainers
 class GigPassApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17");
 
 	@Test
 	void contextLoads() {
