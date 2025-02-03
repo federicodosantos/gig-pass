b116ab0cbfef7e80977fd4476c9adcbdaf2257a9
federico <federicoberto708@gmail.com>
2025-02-03 17:01:16 +0700

    refactor(tokenProvider) : add manual initialization tokenProvider in GigPassApp

--- src/main/java/com/ddosantos/gig/pass/GigPassApplication.java
+++ src/main/java/com/ddosantos/gig/pass/GigPassApplication.java
@@ -1,10 +1,12 @@
 package com.ddosantos.gig.pass;
 
+import com.ddosantos.gig.pass.security.TokenProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +16,10 @@ public class GigPassApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GigPassApplication.class, args);
+	}
+
+	@Bean
+	public TokenProvider tokenProvider() {
+		return new TokenProvider();
 	}
 }
--- src/main/java/com/ddosantos/gig/pass/configuration/AuthenticationConfig.java
+++ src/main/java/com/ddosantos/gig/pass/configuration/AuthenticationConfig.java
@@ -1,21 +1,13 @@
 package com.ddosantos.gig.pass.configuration;
 
+import com.ddosantos.gig.pass.security.TokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AuthenticationConfig {
-
-    private final UserDetailsService userDetailsService;
-
-    public AuthenticationConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
